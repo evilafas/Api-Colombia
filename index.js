d7729f624db5c6e6e626b1c5777aeed239a442f9
EvilAFAS <evilafas99@gmail.com>
2023-05-19 09:43:02 -0500

    Presidentes listos

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const moneda = document.getElementById("moneda");
 const banderaimg = document.getElementById("bandera");
 const departamentos = document.getElementById("departamentos");
 const regiones = document.getElementById("regiones");
+const atracciones = document.getElementById("turismo");
+const presidentes = document.getElementById("presidentes");
+
 async function fetchData(urlApi) {
     const response = await fetch(urlApi, options);
     const data = await response.json()
@@ -27,30 +30,79 @@ async function fetchData(urlApi) {
         lenguaje.innerText = `Lenguajes: ${info.languages}`
         moneda.innerText = `Moneda: ${info.currency}`;
         banderaimg.setAttribute('src', info.flags[0])
+    } catch (error) {
+        console.log("ERROR!")
+    }
+})();
 
+(async () => {
+    try {
         const endPointDepartamentos = 'https://api-colombia.com/api/v1/Department'
         const data = await fetchData(endPointDepartamentos);
         const cards = data.map((item) => {
             return `
             <div class="card">
-            <h1 id="departamento">${item.name}</h1>
-            <p id="descripcionDepartamento">${item.description}</p>
+            <h1 id="tituloCard">${item.name}</h1>
+            <p id="descripcion">${item.description}</p>
           </div>`;
         });
         departamentos.innerHTML = cards.join("");
+    } catch (error) {
+        console.log("ERROR!")
+    }
+})();
 
+(async () => {
+    try {
         const endPointRegiones = 'https://api-colombia.com/api/v1/Region'
         const dataRegiones = await fetchData(endPointRegiones);
         const cardsRegiones = dataRegiones.map((item) => {
             return `
             <div class="card">
-            <h1 id="departamento">${item.name}</h1>
-            <p id="descripcionDepartamento">${item.description}</p>
+            <h1 id="tituloCard">${item.name}</h1>
+            <p id="descripcion">${item.description}</p>
           </div>`;
         });
         regiones.innerHTML = cardsRegiones.join("");
-
     } catch (error) {
+        console.log("ERROR!")
+    }
+})();
 
+(async () => {
+    try {
+        const endPointAtracciones = 'https://api-colombia.com/api/v1/TouristicAttraction'
+        const DataAtracciones = await fetchData(endPointAtracciones);
+        const cardsAtracciones = DataAtracciones.map((item) => {
+            return `
+            <div class="card" id="cardAtracciones">
+            <img src="${item.images}" alt="card Logo"/>
+            <h1>${item.name}</h1>
+            <p>${item.description}</p>
+          </div>`;
+        });
+        atracciones.innerHTML = cardsAtracciones.join("");
+    } catch (error) {
+        console.log("ERROR!: ", error)
+    }
+})();
+
+(async () => {
+    try {
+        const endPointPresidentes = 'https://api-colombia.com/api/v1/President'
+        const DataPresidentes = await fetchData(endPointPresidentes);
+        const cardsPresidentes = DataPresidentes.map((item) => {
+            return `
+            <div class="card" id="cardPresidentes">
+            <img src="${item.image}" alt="card image"/>
+            <h1>${item.id}. ${item.name} ${item.lastName}</h1>
+            <p>${item.startPeriodDate}/${item.endPeriodDate}</p>
+            <p>${item.politicalParty}</p>
+            <p>${item.description}</p>
+            </div>`;
+        });
+        presidentes.innerHTML = cardsPresidentes.join("");
+    } catch (error) {
+        console.log("ERROR!: ", error)
     }
 })();
